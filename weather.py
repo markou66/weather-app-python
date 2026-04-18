25523a371992f9f67bd1bca233b5357e5ca8b28c
Markus <mkvinycios30@gmail.com>
2026-04-18 17:07:01 -0300

    att output

--- weather.py
+++ weather.py
@@ -1,27 +1,38 @@
 from weather_service import get_weather
+from rich.console import Console
+from rich.panel import Panel
+from rich.prompt import Prompt
+
+console = Console()
 
 while True:
-    city = input(str("Cidade: ")).strip()
+    console.print("\n[bold blue]--- Consulta de Clima ---[/bold blue]")
+    
+    
+    city = Prompt.ask("[bold white]Digite a cidade[/bold white]").strip()
 
-    while city == "":
-        print('Digite uma cidade...')
-        city = input("Cidade: ").strip()
+    if not city:
+        console.print("[yellow]Por favor, digite o nome de uma cidade![/yellow]")
+        continue
 
-
-
-    weather = get_weather(city)
+    with console.status("[bold green]Buscando dados...[/bold green]"):
+        weather = get_weather(city)
 
     if weather:
-        print(f"{weather['cidade']}")
-        print(f"{weather['temp']:.1f}°C - {weather['descricao']}")
-        print(f"Máx: {weather['max']}°C | Mín: {weather['min']}°C")
-        print(f"Sensação: {weather['sensacao']}°C")
-        print(f"Umidade: {weather['umidade']}%")
+        conteudo = (
+            f"[bold cyan] {weather['cidade']}[/bold cyan]\n"
+            f"[yellow] Temperatura:[/yellow] {weather['temp']:.1f}°C\n"
+            f"[white] Condição:[/white]    {weather['descricao'].capitalize()}\n"
+            f"[blue] Umidade:[/blue]      {weather['umidade']}%\n"
+            f"---"
+            f"\n[red]🔺 Máx:[/red] {weather['max']}°C | [blue]🔻 Mín:[/blue] {weather['min']}°C"
+        )
+        
+        console.print(Panel(conteudo, title="[bold]Previsão do Tempo[/bold]", border_style="green", expand=False))
     else:
-        print("Erro ao buscar dados")
+        console.print(Panel("[red]❌ Erro:[/red] Não foi possível encontrar essa cidade.", border_style="red"))
 
-    again = input("\nDeseja consultar outra cidade? (s/n): ").strip().lower()
-
+    again = Prompt.ask("\nDeseja consultar outra?", choices=["s", "n"], default="s")
     if again != "s":
-        print("Encerrando...")
+        console.print("[bold yellow]Encerrando... [/bold yellow]")
         break
